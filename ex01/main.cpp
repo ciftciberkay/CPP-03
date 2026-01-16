b6656da46510456d3474a748c4fdb77470339560
ciftciberkay <berko.berko2016@gmail.com>
2026-01-17 02:02:09 +0300

    edited main.cpp (ex01 ex02)

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,13 +2,10 @@
 
 int main()
 {
-    std::cout << "--- CONSTRUCTION ---" << std::endl;
-    ScavTrap player("Serena");
-    std::cout << "--- ACTIONS ---" << std::endl;
-    player.attack("Target-Dummy");
+    ScavTrap player("Alex");
+    player.attack("Mesut");
     player.takeDamage(10);
     player.beRepaired(10);
     player.guardGate();
-    std::cout << "--- DESTRUCTION ---" << std::endl;
     return 0;
 }
